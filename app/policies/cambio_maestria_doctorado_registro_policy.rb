b640cb879e0571f6d30d344ca9d47ebb99124ad1
luis <luis@localhost.localdomain>
2020-07-02 00:36:20 -0500

    Politicas para modelos terminados

---
+++ app/policies/cambio_maestria_doctorado_registro_policy.rb
@@ -0,0 +1,31 @@
+class CambioMaestriaDoctoradoRegistroPolicy < ApplicationPolicy
+
+  def index?
+    user.administrador? or user.coordinador? or user.asuntos_escolares? or user.colegio_profesores?
+  end
+
+  def show?
+    user.administrador? or user.coordinador? or user.asuntos_escolares? or user.colegio_profesores?
+  end
+
+  def create?
+    user.administrador? or user.usuario?
+  end
+
+  def new?
+    user.administrador? or user.usuario?
+  end
+
+  def update?
+    user.administrador? or user.coordinador? or user.asuntos_escolares? or user.colegio_profesores?
+  end
+
+  def edit?
+    user.administrador? or user.coordinador? or user.asuntos_escolares? or user.colegio_profesores?
+  end
+
+  def destroy?
+    user.administrador?
+  end
+
+end
